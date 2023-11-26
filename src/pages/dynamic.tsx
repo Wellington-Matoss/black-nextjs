774002bf3073a72210856e955f7eee02e5b824c9
Wellington-Matoss <wellington.matos.neves@gmail.com>
2023-11-26 11:01:17 -0300

    att

--- src/pages/dynamic.tsx
+++ src/pages/dynamic.tsx
@@ -7,6 +7,16 @@ type ApiResponse = {
   timestamp: Date;
 };
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverSideData: ApiResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_APIURL}/api/hello`
+  ).then((res) => res.json());
+
+  return {
+    props: serverSideData,
+  };
+};
+
 const Dynamic: NextPage = (props: {
   children?: ReactNode;
   serverSideData?: ApiResponse;
@@ -24,17 +34,17 @@ const Dynamic: NextPage = (props: {
 
   return (
     <Container tag="main">
-      <h1 className="my-5">Como funcionam as as renderizações do Next.js</h1>
+      <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
 
       <Row>
         <Col>
-          <h3>
-            Gerado no servidor: {props.serverSideData?.timestamp.toString()}
-          </h3>
+          <h3>Gerado no servidor:</h3>
+          <h2>{props.serverSideData?.timestamp.toString()}</h2>
         </Col>
 
         <Col>
-          <h3>Gerado no cliente: {clientSideData?.timestamp.toString()}</h3>
+          <h3>Gerado no cliente:</h3>
+          <h2>{clientSideData?.timestamp.toString()}</h2>
         </Col>
       </Row>
     </Container>
